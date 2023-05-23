72bbc64630b78634b72f2739a6ca63a3b2fc6480
Ali From NocEmam <alimohammadzade7976@gmail.com>
2023-05-23 07:47:12 +0330

    Edit signalRAgent file

--- ServiceHosts/wwwroot/js/signalRAgent.js
+++ ServiceHosts/wwwroot/js/signalRAgent.js
@@ -1,7 +1,4 @@
-﻿
-
-
-var chatFormEl;
+﻿var chatFormEl;
 var requestAnchors;
 var TableAllUsers;
 var ulFriendsEl;
@@ -13,11 +10,12 @@ var roomListEl = document.getElementById('roomList');
 var chatHistoryDiv = document.getElementById('chatHistory');
 var activeUserIdToChat = 0;
 var currentTableRowInAllUsersWorkOnIt;
+
+
 //Initial chatHubConnection
 var chatConnection = new signalR.HubConnectionBuilder()
     .withUrl('/chatHub')
     .build();
-
 
 chatConnection.onclose(function () {
     //Try to reconnect 5 time
@@ -209,6 +207,7 @@ function appendChatItems(chatItems) {
 
 }
 
+//Appand New Message to chat history
 function appendChatItem(message) {
     if (!message && message.length == 0)
         return;
@@ -288,7 +287,7 @@ function removeAllChildren(_node) {
         _node.removeChild(_node.lastChild);
     }
 }
-var activeUserToChat;
+
 //switch active tab to the given tab
 function SwitchTabTo(liHrefAtt) {
     var divTabToActive = document.getElementById('divTabToActive');
@@ -462,209 +461,3 @@ function ready() {
 
 document.addEventListener('DOMContentLoaded', ready);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function loadChatList(userNameList) {
-//    debugger;
-//    if (!userNameList.length)
-//        return;
-//    const userNames = JSON.parse(userNameList);
-//    loadRooms(userNames);
-//}
-
-
-
-//function sendMessage(text) {
-//    if (text && text.length) {
-//        // TODO: Send an agent message
-//    }
-//}
-
-
-
-//function switchActiveRoomTo(id) {
-//    if (id === activeRoomId) return;
-
-//    if (activeRoomId) {
-//        // TODO: Leave the room
-//    }
-
-//    activeRoomId = id;
-//    removeAllChildren(chatHistoryDiv);
-
-//    if (!id) return;
-
-//    // TODO: Join the room
-//    // TODO: Load the room history
-//}
-
-
-
-
-////roomListEl.addEventListener('click', function (e) {
-////    chatHistoryDiv.style.display = 'block';
-
-////    setActiveRoomButton(e.target);
-
-////    var roomId = e.target.getAttribute('data-id');
-////    switchActiveRoomTo(roomId);
-////});
-
-//function setActiveRoomButton(el) {
-//    var allButtons = roomListEl.querySelectorAll('a.list-group-item');
-
-//    allButtons.forEach(function (btn) {
-//        btn.classList.remove('active');
-//    });
-
-//    el.classList.add('active');
-//}
-
-//function loadRooms(rooms) {
-//    if (!rooms.length) return;
-
-
-//    switchActiveRoomTo(null);
-//    removeAllChildren(roomListEl);
-
-//    for (var i = 0; i < rooms.length; i++) {
-//        var item = rooms[i];
-//        var currentId = item.Id;
-//        var currentName = item.UserName;
-//        if (!currentName) continue;
-
-//        var roomButton = createRoomButton(currentId, currentName);
-//        roomListEl.appendChild(roomButton);
-//    }
-//    //rooms.forEach(function (item) {
-//    //    var currentId = item.id;
-//    //    var currentName = item.name;
-//    //    if (!currentName) return;
-
-//    //    var roomButton = createRoomButton(currentId, currentName);
-//    //    roomListEl.appendChild(roomButton);
-//    //});
-//}
-
-//function createRoomButton(id, userName) {
-//    var anchorEl = document.createElement('a');
-//    anchorEl.className = 'list-group-item list-group-item-action d-flex justify-content-between align-items-center';
-//    anchorEl.setAttribute('data-id', id);
-//    anchorEl.textContent = userName;
-//    anchorEl.href = '#';
-
-//    return anchorEl;
-//}
-
-//function addMessages(messages) {
-//    if (!messages) return;
-
-//    messages.forEach(function (m) {
-//        addMessage(m.senderName, m.sentAt, m.text);
-//    });
-//}
-
-//function addMessage(name, time, message) {
-//    var nameSpan = document.createElement('span');
-//    nameSpan.className = 'name';
-//    nameSpan.textContent = name;
-
-//    var timeSpan = document.createElement('span');
-//    timeSpan.className = 'time';
-//    var friendlyTime = moment(time).format('H:mm');
-//    timeSpan.textContent = friendlyTime;
-
-//    var headerDiv = document.createElement('div');
-//    headerDiv.appendChild(nameSpan);
-//    headerDiv.appendChild(timeSpan);
-
-//    var messageDiv = document.createElement('div');
-//    messageDiv.className = 'message';
-//    messageDiv.textContent = message;
-
-//    var newItem = document.createElement('li');
-//    newItem.appendChild(headerDiv);
-//    newItem.appendChild(messageDiv);
-
-//    chatHistoryDiv.appendChild(newItem);
-//    chatHistoryDiv.scrollTop = chatHistoryDiv.scrollHeight - chatHistoryDiv.clientHeight;
-//}
-
-//function removeAllChildren(node) {
-//    if (!node) return;
-
-//    while (node.lastChild) {
-//        node.removeChild(node.lastChild);
-//    }
-//}
-
-/*chatConnection.on('responseSetName', setNameResult);*/
-//chatConnection.on('ReceiveMessage', addMessage);
-//chatConnection.on('loadUserNamesChatWithThem', loadChatList);
-// TODO: Initialize hub connections
-
-
-////make a function to start connection 
-
-//function S() {
-//    var conresult = '';
-//    chatConnection.start()
-//        .catch(function (err) {
-//            console.alert("!!!Atention Error:\n" + err);
-//            conresult = err;
-//        });
-//    if (conresult.length > 0) {
-
-//    } else {
-//        openChatSegment();
-//    }
-
-//}
-
-//make a handle connection when connection is disconnected
-//chatConnection.onclose(function () {
-//    handleDisconnected();
-//});
-
-////handle changes of page after connection started
-//function openChatSegment() {
-
-//    var enterYourNameWarningEl = document.getElementById('enterNameInfo');
-//    enterYourNameWarningEl.style.display = 'none';
-//}
-
-//public method give handler to retry connect again to server
-//function handleDisconnected(retryFunc) {
-//    container.classList.add('blured');
-//    console.log('Reconnecting in 5 seconds...');
-//    setTimeout(retryFunc, 5000);
-//}
-
-//function setNameResult(result) {
-//    if (result) {
-//        //if the user exist before
-//        //ToDO: implement add history of chat
-//    } else {
-//        //if the user isn't exit before
-//        const message = 'Hi Dear ' + currentUserName + ' welcome to the ChatRoom';
-//        const timeOffsetUtcNow = new Date().getTimezoneOffset(0);
-//        addMessage(currentUserName, timeOffsetUtcNow, message);
-//    }
-//}
-//UserForm.addEventListener('submit', function (e) {
-//    e.preventDefault();
-//    chatConnection.invoke('SetName', currentUserName)
-//        .catch((err) => alert("An exception accord on server side..."));
-//    openChatSegment();
-//});
